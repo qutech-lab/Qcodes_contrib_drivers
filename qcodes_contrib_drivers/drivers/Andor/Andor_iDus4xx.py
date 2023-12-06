d4c5ef8f092bb1236c026f60510c18b426dd318d
Tobias Hangleiter (Valhalla) <tobias.hangleiter@rwth-aachen.de>
2023-12-06 12:36:28 +0100

    Add warm_up() method.

--- qcodes_contrib_drivers/drivers/Andor/Andor_iDus4xx.py
+++ qcodes_contrib_drivers/drivers/Andor/Andor_iDus4xx.py
@@ -978,3 +978,33 @@ class AndorIDus4xx(Instrument):
             pbar.postfix = status
             pbar.n = pbar.total
             pbar.refresh()
+
+    def warm_up(self, target: int = 15, show_progress: bool = True) -> None:
+        """Turn the cooler off and wait for the temperature to reach target.
+
+        Parameters
+        ----------
+        target : int, optional
+            The target temperature. Defaults to 15C.
+        show_progress : bool, optional
+            Show a progressbar. The default is True.
+
+        """
+        self.cooler.set('off')
+
+        # bar does not show for negative totals, but ok
+        with tqdm(
+                initial=(initial := self.temperature.get()),
+                total=target - initial,
+                desc=f'{self.name} warming up to {target}{self.temperature.unit}',
+                unit=self.temperature.unit,
+                disable=not show_progress
+        ) as pbar:
+            while (temp := self.temperature.get()) != target:
+                # For lack of a better method:
+                # https://github.com/tqdm/tqdm/issues/1264
+                pbar.n = temp - target
+                pbar.refresh()
+                time.sleep(1)
+            pbar.n = pbar.total
+            pbar.refresh()
